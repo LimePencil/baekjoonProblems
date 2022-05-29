adefe759b7af053de0922a3099623098111a6fdf
limepencil <jaeyoungshin03@gmail.com>
2022-05-29 12:41:23 +0700

    may 29

---
+++ com/LimePencil/Q11778/피보나치_수와_최대공약수.py
@@ -0,0 +1,42 @@
+from math import gcd
+import sys
+
+def find_fib(n):
+    def pow(n_arr,exp):
+        if exp ==1:
+            return arr
+        
+        new_arr = pow(n_arr,exp//2)
+        new_arr = mul(new_arr,new_arr)
+
+        if exp%2 ==1:
+            new_arr = mul(new_arr,arr)
+        return new_arr
+    
+    def mul(arr1,arr2):
+        arr_ans = [[0 for _ in range(2)]for _ in range(2)]
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                    arr_ans[i][j] += arr1[i][k] * arr2[k][j]
+                    arr_ans[i][j] %= 1000000007
+        return arr_ans
+    if n==0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n== 2:
+        return 1
+    elif n == 3:
+        return 2
+    else:
+        arr = [[1,1],[1,0]]
+        new_arr = pow(arr,n-1)
+        return new_arr[0][0]
+
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+
+a,b=map(int,input().split())
+print((find_fib(gcd(a,b))))
---
+++ com/LimePencil/Q13075/Fibonacci_Sequence.py
@@ -0,0 +1,40 @@
+import sys
+
+def pow(n_arr,exp):
+    if exp ==1:
+        return arr
+    
+    new_arr = pow(n_arr,exp//2)
+    new_arr = mul(new_arr,new_arr)
+
+    if exp%2 ==1:
+        new_arr = mul(new_arr,arr)
+    return new_arr
+    
+
+def mul(arr1,arr2):
+    arr_ans = [[0 for _ in range(n)]for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                arr_ans[i][j] += arr1[i][k] * arr2[k][j]
+                arr_ans[i][j] %= 1000000000
+    return arr_ans
+
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+for _ in range(int(input())):
+    a = int(input())
+    if a==0:
+        print("0")
+    elif a == 1:
+        print("1")
+    elif a== 2:
+        print("1")
+    elif a == 3:
+        print("2")
+    else:
+        n = 2
+        arr = [[1,1],[1,0]]
+        new_arr = pow(arr,a-1)
+        print(new_arr[0][0])
