2e308f9e094e3f15ee9f410cc13b2df4b50c1412
limepencil <jaeyoungshin03@gmail.com>
2022-06-22 10:07:47 +0900

    june 22

---
+++ com/LimePencil/Q24265/알고리즘_수업_-_알고리즘의_수행_시간_4.py
@@ -0,0 +1,6 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+print(n*(n-1)//2)
+print(2)
---
+++ com/LimePencil/Q24267/알고리즘_수업_-_알고리즘의_수행_시간_6.py
@@ -0,0 +1,6 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+print(n*(n-1)*(n-2)//6)
+print(3)
