2410d8b3c64ec818b8df0305f3554616894d2666
limepencil <jaeyoungshin03@gmail.com>
2022-07-10 20:12:32 +0900

    fix

--- competitions/Semi-Game Cup 3/A.py
+++ competitions/Semi-Game Cup 3/A.py
@@ -2,7 +2,6 @@ import math
 import sys
 
 input=sys.stdin.readline
-
 n,k=map(int,input().split())
-mod=int(10e9+7)
-print(pow(2,k-1,mod)*math.comb(n,k)%mod)
+mod=int(1e9+7)
+print((pow(2,k-1,mod)%mod*math.comb(n,k)%mod)%mod)
