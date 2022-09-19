3cfbcc02b8bf1baa12263b55a89d8018dd53e4b9
limepencil <jaeyoungshin03@gmail.com>
2022-09-20 01:00:26 +0900

    sep 19

---
+++ com/LimePencil/Q1679/숫자놀이.py
@@ -0,0 +1,19 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+n = int(input())
+l=list(map(int,input().split()))
+nums = set(l)
+k = int(input())
+dp=[float('inf') for _ in range(l[-1]*k+2)]
+dp[1]=1
+for i in range(2,l[-1]*k+2):
+    if i in nums:
+        dp[i]=1
+    else:
+        for j in range(1,(i+1)//2+1):
+            dp[i]=min(dp[i],dp[j]+dp[i-j])
+    if dp[i]>k:
+        s="holsoon" if i%2==0 else "jjaksoon"
+        print(f"{s} win at {i}")
+        break
