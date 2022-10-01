8ea9eb61bac4b9fe3b4f1ae40ff972f67c2864dd
limepencil <jaeyoungshin03@gmail.com>
2022-10-01 23:01:52 +0900

    oct 1

--- com/LimePencil/Q17615/볼_모으기.py
+++ com/LimePencil/Q17615/볼_모으기.py
@@ -1,65 +1,26 @@
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
+def move_balls(type_of_ball_to_move,s):
+    # To consider: red to left, red to right, blue to left, blue to right
+    part_of_big_group=False
+    cnt=0
+    for i in range(n):
+        if s[i]==type_of_ball_to_move:
+            if i==0:
+                part_of_big_group=True
+            elif part_of_big_group:
+                continue
+            else:
+                cnt+=1
+        else:
+            part_of_big_group=False
+    return cnt
+
 n = int(input())
 s=input()
-ans=float('inf')
-# red to left
-part_of_big_group=False
-cnt=0
-for i in range(n):
-    if s[i]=="R":
-        if i==0:
-            part_of_big_group=True
-        elif part_of_big_group:
-            continue
-        else:
-            cnt+=1
-    else:
-        part_of_big_group=False
-ans=min(ans,cnt)
-
-# red to right
-part_of_big_group=False
-cnt=0
-for i in range(n-1,-1,-1):
-    if s[i]=="R":
-        if i==n-1:
-            part_of_big_group=True
-        elif part_of_big_group:
-            continue
-        else:
-            cnt+=1
-    else:
-        part_of_big_group=False
-ans=min(ans,cnt)
-# blue to left
-part_of_big_group=False
-cnt=0
-for i in range(n):
-    if s[i]=="B":
-        if i==0:
-            part_of_big_group=True
-        elif part_of_big_group:
-            continue
-        else:
-            cnt+=1
-    else:
-        part_of_big_group=False
-ans=min(ans,cnt)
-
-# blue to right
-part_of_big_group=False
-cnt=0
-for i in range(n-1,-1,-1):
-    if s[i]=="B":
-        if i==n-1:
-            part_of_big_group=True
-        elif part_of_big_group:
-            continue
-        else:
-            cnt+=1
-    else:
-        part_of_big_group=False
-ans=min(ans,cnt)
-print(ans)
+print(min(
+    move_balls("R",s),
+    move_balls("R",s[::-1]),
+    move_balls("B",s),
+    move_balls("B",s[::-1])))
