4689f230ff848e3139f4b03235f9dbf7ce7b9538
limepencil <jaeyoungshin03@gmail.com>
2022-10-01 23:42:05 +0900

    oct 1

--- com/LimePencil/Q17615/볼_모으기.py
+++ com/LimePencil/Q17615/볼_모으기.py
@@ -2,23 +2,12 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 def move_balls(type_of_ball_to_move,s):
-    # To consider: red to left, red to right, blue to left, blue to right
-    part_of_big_group=False
-    cnt=0
-    for i in range(n):
-        if s[i]==type_of_ball_to_move:
-            if i==0:
-                part_of_big_group=True
-            elif part_of_big_group:
-                continue
-            else:
-                cnt+=1
-        else:
-            part_of_big_group=False
-    return cnt
+    s=s.lstrip(type_of_ball_to_move)
+    return s.count(type_of_ball_to_move)
 
 n = int(input())
 s=input()
+# To consider: red to left, red to right, blue to left, blue to right
 print(min(
     move_balls("R",s),
     move_balls("R",s[::-1]),
