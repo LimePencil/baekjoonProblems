aa4668b032434c675163d28fb6441aa2a27a5f9d
limepencil <jaeyoungshin03@gmail.com>
2022-10-05 22:00:49 +0900

    update

--- com/LimePencil/Q1679/숫자놀이.py
+++ com/LimePencil/Q1679/숫자놀이.py
@@ -6,12 +6,11 @@ l=list(map(int,input().split()))
 nums = set(l)
 k = int(input())
 dp=[float('inf') for _ in range(l[-1]*k+2)]
-dp[1]=1
-for i in range(2,l[-1]*k+2):
+for i in range(1,l[-1]*k+2):
     if i in nums:
         dp[i]=1
     else:
-        for j in range(1,(i+1)//2+1):
+        for j in range(1,i//2+1):
             dp[i]=min(dp[i],dp[j]+dp[i-j])
     if dp[i]>k:
         s="holsoon" if i%2==0 else "jjaksoon"
