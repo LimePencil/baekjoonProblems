d515422a7390ee563fe7e21c8f103b7bdd0e8c9c
limepencil <jaeyoungshin03@gmail.com>
2022-11-05 00:03:08 +0900

    nov 4

---
+++ com/LimePencil/Q25932/Find_the_Twins.py
@@ -0,0 +1,10 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+for _ in range(n):
+    arr = list(map(int,input().split()))
+    print(*arr)
+    print("both" if 17 in arr and 18 in arr else "zack" if 17 in arr else "mack" if 18 in arr else "none")
+    print()
