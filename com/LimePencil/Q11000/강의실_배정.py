05e4d1476eea785ea363a8fffae91c531d4d2cb2
Jaeyoung Shin <jaeyoungshin03@gmail.com>
2022-12-15 14:42:03 +0900

    dec 15

---
+++ com/LimePencil/Q11000/강의실_배정.py
@@ -0,0 +1,19 @@
+import sys
+import heapq
+
+input = lambda: sys.stdin.readline().rstrip()
+n = int(input())
+rooms=[]
+by_start=[]
+for _ in range(n):
+    s,e = list(map(int,input().split()))
+    heapq.heappush(by_start,(s,e))
+ans=0
+for _ in range(n):
+    prev_class=-1
+    s,e=heapq.heappop(by_start)    
+    while len(rooms)>0 and  rooms[0]<=s:
+        heapq.heappop(rooms)
+    heapq.heappush(rooms,e)
+    ans=max(ans,len(rooms))
+print(ans)
