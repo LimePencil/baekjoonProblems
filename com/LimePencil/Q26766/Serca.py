fd41faf43c80225b491830f7f9f6b81dd2da9d23
Jaeyoung Shin <jaeyoungshin03@gmail.com>
2022-12-27 10:14:21 +0900

    dec 27

---
+++ com/LimePencil/Q26766/Serca.py
@@ -0,0 +1,6 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+n = int(input())
+for _ in range(n):   
+    print(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     ")
