157c90b838f2e91e1198ced597cb1ca1a8f5de68
Jaeyoung Shin <jaeyoungshin03@gmail.com>
2023-01-21 02:09:55 +0900

    jan 20

---
+++ com/LimePencil/Q26772/Poziome_serca.py
@@ -0,0 +1,13 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+n = int(input())
+print(" @@@   @@@  "*n)
+print("@   @ @   @ "*n)
+print("@    @    @ "*n)
+print("@         @ "*n)
+print(" @       @  "*n)
+print("  @     @   "*n)
+print("   @   @    "*n)
+print("    @ @     "*n)
+print("     @      "*n)
